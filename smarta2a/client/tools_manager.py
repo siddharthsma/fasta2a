9bff8f953f2bb04e955ae7e825adfc4611af2a56
Siddharth Ambegaonkar <siddharthsma@gmail.com>
2025-05-02 15:49:58 +0100

    Fix bugs

--- smarta2a/client/tools_manager.py
+++ smarta2a/client/tools_manager.py
@@ -38,11 +38,17 @@ class ToolsManager:
 
     def describe_tools(self, client_type: Literal["mcp", "a2a"]) -> str:
         lines = []
+        print(self.tools_list)
         for tool in self.tools_list:
-            schema = json.dumps(tool.get("input_schema", {}), indent=2)
-            lines.append(
-                f"- **{tool.get('name')}**: {tool.get('description', '')}\n  Parameters schema:\n  ```json\n{schema}\n```"
-            )
+            if client_type == "mcp" and isinstance(tool, MCPClient):
+                schema = json.dumps(tool.input_schema, indent=2)
+                lines.append(
+                    f"- **{tool.name}**: {tool.description}\n  Parameters schema:\n  ```json\n{schema}\n```"
+                )
+            elif client_type == "a2a" and isinstance(tool, A2AClient):
+                lines.append(
+                    f"- **{tool.name}**: {tool.description} Parameters schema:\n  ```json\n{schema}\n```"
+                )
         return "\n".join(lines)
 
     def get_client(self, tool_name: str) -> Any:
